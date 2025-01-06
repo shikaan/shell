2f65c6dbd914086d427f3ef52269fce66ec8b5ce
Manuel Spagnolo <manuel.spagnolo@proton.me>
2025-01-07 00:34:40 +0100

    support simple output redirect

--- shell.c
+++ shell.c
@@ -1,13 +1,14 @@
 #include "builtin.h"
 #include "process.h"
 #include "utils.h"
+#include "string.h"
 
 #include "sds/sds.h"
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 
 #define PROMPT "$ "
 
@@ -23,17 +24,47 @@ sds read_line() {
   }
 }
 
+const char OUTPUT_REDIRECT = '>';
+const char* CLEANUP_CHARS = " >";
+sds *parse_line(sds line, int *argc, int *out) {
+  const int line_length = sdslen(line);
+  int redirect_index = string_find(line, OUTPUT_REDIRECT);
+
+  // There is no redirect symbol, send output to STDOUT
+  if (redirect_index == -1) {
+    sds *argv = sdssplitargs(line, argc);
+    *out = STDOUT_FILENO;
+    return argv;
+  }
+
+  sds *split = string_split_at(line, redirect_index);
+
+  split[1] = sdstrim(split[1], CLEANUP_CHARS);
+  *out = open(split[1], O_RDWR | O_CREAT | O_TRUNC, 0666);
+  if (*out == -1)
+    goto error;
+
+  split[0] = sdstrim(split[0], CLEANUP_CHARS);
+  sds *argv = sdssplitargs(split[0], argc);
+  sdsfreesplitres(split, 2);
+  return argv;
+
+error:
+  perror(NAME);
+  sdsfreesplitres(split, 2);
+  return NULL;
+}
+
 int main(int argc, char **argv) {
-  int out = STDOUT_FILENO;
   int status = 0;
 
-   while (1) {
+  while (1) {
     printf(PROMPT);
     sds line = read_line();
+    int argc = 0, out = STDOUT_FILENO;
+    sds *argv = parse_line(line, &argc, &out);
 
-    int argc = 0;
-    sds *argv = sdssplitargs(line, &argc);
-    if (argc != 0) {
+    if (argv && argc != 0) {
       Builtin b = builtin_from(argv[0]);
 
       if (b == BUILTIN_UNDEFINED) {
